fd1db0471501671f114879145a94e0367d066401
othmanemoumen <yvvno90@gmail.com>
2025-05-14 21:12:09 +0200

    Update loader.js

--- assets/javascripts/loader.js
+++ assets/javascripts/loader.js
@@ -1,12 +1,9 @@
 window.addEventListener("load", () => {
   const loader = document.querySelector(".loader");
 
-  // Optional: keep the loader visible for a short time to avoid flashing
-  setTimeout(() => {
-    loader.classList.add("loader--hidden");
+  loader.classList.add("loader--hidden");
 
-    loader.addEventListener("transitionend", () => {
-      loader.remove(); // Or: document.body.removeChild(loader);
-    });
-  }, 500); // 500ms delay before fading out
+  loader.addEventListener("transitionend", () => {
+    document.body.removeChild(loader);
+  });
 });
