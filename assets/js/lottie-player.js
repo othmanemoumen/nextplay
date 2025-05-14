65df577e326f520bc703526ea2f270ae0ff41770
othmanemoumen <yvvno90@gmail.com>
2025-05-14 02:35:44 +0200

    Update lottie-player.js

--- assets/js/lottie-player.js
+++ assets/js/lottie-player.js
@@ -1,1 +1,10 @@
 
+document.addEventListener("DOMContentLoaded", function () {
+  lottie.loadAnimation({
+    container: document.getElementById('lottie'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'assets/animations/animation.json'
+  });
+});
