9ab3f6508daa97b2ebb3c28fe16c6dce75ec956c
othmanemoumen <yvvno90@gmail.com>
2025-05-14 16:02:28 +0200

    Delete assets/js/scripts directory

--- assets/js/scripts/lottie.js
+++
@@ -1,9 +0,0 @@
-
-// This JS will be loaded from index.html
-lottie.loadAnimation({
-  container: document.getElementById('lottie'),
-  renderer: 'svg',
-  loop: true,
-  autoplay: true,
-  path: 'https://lottie.host/4a78edf8-2c55-4f9d-bc76-fb4fc006f793/nYXz1Pid8D.json'
-});
