2127bc2f1041d7b9b7a4fe10520dcaadf1e09e1f
othmanemoumen <yvvno90@gmail.com>
2025-05-14 19:31:47 +0200

    Update loader.js

--- assets/javascripts/loader.js
+++ assets/javascripts/loader.js
@@ -1,18 +1,9 @@
 window.addEventListener("load", () => {
   const loader = document.querySelector(".loader");
-  if (!loader) return;
 
   loader.classList.add("loader--hidden");
 
-  // Fallback: remove after transition even if event doesn't fire
-  const removeLoader = () => {
-    if (loader && loader.parentNode) {
-      loader.remove();
-    }
-  };
-
-  loader.addEventListener("transitionend", removeLoader);
-
-  // Fallback: remove after 1s just in case
-  setTimeout(removeLoader, 1000);
+  loader.addEventListener("transitionend", () => {
+    document.body.removeChild(loader);
+  });
 });
